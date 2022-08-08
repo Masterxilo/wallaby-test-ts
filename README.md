36f3df1a1acd694e6cb398b2c7bbff22c150b3e6
Paul Frischknecht <paulfrischknechtmx@gmail.com>
2022-08-09 00:54:25 +0200

    more

--- README.md
+++ README.md
@@ -41,3 +41,14 @@ Tested with
 
 * Visual Studio Code
 * IntelliJ IDEA Ultimate 2021
+
+
+
+# Solution
+```
+expect(isSingleDigitExcept5(-1)).toBe(false);
+expect(isSingleDigitExcept5(0)).toBe(true);
+expect(isSingleDigitExcept5(5)).toBe(false);
+expect(isSingleDigitExcept5(9)).toBe(true);
+expect(isSingleDigitExcept5(12)).toBe(false);
+```
--- src/lib.spec.ts
+++ src/lib.spec.ts
@@ -1,10 +1,11 @@
-import { isSingleDigit } from "./lib";
+import { isSingleDigitExcept5 } from "./lib";
 
 describe('isSingleDigit', () => {
     it('should classify correctly', function () {
-        expect(isSingleDigit(-1)).toBe(false);
-        expect(isSingleDigit(0)).toBe(true);
-        expect(isSingleDigit(9)).toBe(true);
-        expect(isSingleDigit(12)).toBe(false);
+        expect(isSingleDigitExcept5(-1)).toBe(false);
+        expect(isSingleDigitExcept5(0)).toBe(true);
+        expect(isSingleDigitExcept5(5)).toBe(false);
+        expect(isSingleDigitExcept5(9)).toBe(true);
+        expect(isSingleDigitExcept5(12)).toBe(false);
     });
 });

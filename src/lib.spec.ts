99dbf78472b7a919f2a5c9294c0f5c54e70a1a11
Paul Frischknecht <paulfrischknechtmx@gmail.com>
2022-08-09 00:54:38 +0200

    reset

--- src/lib.spec.ts
+++ src/lib.spec.ts
@@ -2,10 +2,5 @@ import { isSingleDigitExcept5 } from "./lib";
 
 describe('isSingleDigit', () => {
     it('should classify correctly', function () {
-        expect(isSingleDigitExcept5(-1)).toBe(false);
-        expect(isSingleDigitExcept5(0)).toBe(true);
-        expect(isSingleDigitExcept5(5)).toBe(false);
-        expect(isSingleDigitExcept5(9)).toBe(true);
-        expect(isSingleDigitExcept5(12)).toBe(false);
     });
 });

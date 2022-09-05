47e61bbc509f484c77a68e9f39d07b48571eed59
Paul Frischknecht <paulfrischknecht@paulfrischknecht.ch>
2022-09-05 19:07:37 +0200

    customizations

--- run.ps1
+++ run.ps1
@@ -1,6 +1,13 @@
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
+try {
+    Get-Command npm 
+} catch {
+    echo "nodejs is missing, please install as admin:"
+    choco install -y nodejs
+}
+
 function check() {
     &$args[0] @($args[1..($args.length-1)])
     if ($LASTEXITCODE -ne 0) { throw $LASTEXITCODE }

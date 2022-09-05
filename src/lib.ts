04ab3bc45186e138311b9dd9e0bdaf67ac1b6dc4
Paul Frischknecht <paulfrischknecht@paulfrischknecht.ch>
2022-09-05 21:18:06 +0200

    ready to present

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,6 @@
 export function isSingleDigitExcept5(x: number) {
     if (x >= 0 && x < 9) {
-        return x != 5 ? true : false;
+        return x != 5;
     } else {
         return false;
     }

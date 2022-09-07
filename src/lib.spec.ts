2f8e51a3999fc885efc6592300d65b9b8b08d992
Paul Frischknecht <paulfrischknecht@paulfrischknecht.ch>
2022-09-07 08:23:08 +0200

    solution

--- src/lib.spec.ts
+++ src/lib.spec.ts
@@ -3,5 +3,10 @@ import { isSingleDigitExcept5 } from "./lib";
 describe('isSingleDigit', () => {
     it('should classify correctly', function () {
         /// expect(isSingleDigitExcept5(...)).toBe(true|false);
+        expect(isSingleDigitExcept5(0)).toBe(true);
+        expect(isSingleDigitExcept5(10)).toBe(false);
+        expect(isSingleDigitExcept5(5)).toBe(false);
+        expect(isSingleDigitExcept5(-1)).toBe(false);
+        expect(isSingleDigitExcept5(9)).toBe(true);
     });
 });
--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,5 @@
 export function isSingleDigitExcept5(x: number) {
-    if (x >= 0 && x < 9) {
+    if (x >= 0 && x <= 9) {
         return x != 5;
     } else {
         return false;
